716496dabd498916b568547a06e8f0af0353a322
Yunes B <yunes.bakhush@gmail.com>
2022-05-09 17:57:22 +0600

    feat: prettierrc and mockdata added

--- example/index.tsx
+++ example/index.tsx
@@ -2,25 +2,12 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Parser } from '../.';
+import mockData from './utils/mockData';
 
 const App = () => {
   return (
     <div>
-      <Parser
-        data={{
-          time: 1652089479683,
-          blocks: [
-            {
-              id: 'CMci-PoqbJ',
-              type: 'header',
-              data: {
-                text: 'Editor.js',
-                level: 2,
-              },
-            },
-          ],
-        }}
-      />
+      <Parser data={mockData} />
     </div>
   );
 };
